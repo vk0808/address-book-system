ff913580cfb79fd0a1d176def74a6cab0d523890
vk0808 <vk4765712@gmail.com>
2021-09-15 12:18:57 +0530

    feat: add stored procedure

---
+++ AddressBook-ADO.NET/StoredProcedure.sql
@@ -0,0 +1,32 @@
+﻿CREATE PROCEDURE [dbo].[spUpdatePersonCityState]
+	@BookID INT,
+	@City VARCHAR(100),
+	@State VARCHAR(100)
+AS
+BEGIN
+SET XACT_ABORT ON;
+BEGIN TRY
+BEGIN TRANSACTION;
+	UPDATE address
+	SET City = @City, State = @State
+	WHERE BookID = @BookID;
+	SELECT BookID, City, State
+	FROM address WHERE BookID = @BookID;
+	COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+SELECT ERROR_NUMBER() AS ErrorNumber, ERROR_MESSAGE() AS ErrorMessage;
+IF(XACT_STATE()) = -1
+	BEGIN
+		PRINT N'The transaction is in an uncommitable state.'+'Rolling back transaction.'
+		ROLLBACK TRANSACTION;
+	END;
+
+IF(XACT_STATE()) = 1
+	BEGIN
+		PRINT N'The transaction is committable. '+'Committing transaction.'
+		COMMIT TRANSACTION;
+	END;
+END CATCH
+END
+GO
